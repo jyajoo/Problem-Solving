ba307eef265f091ab01816c4ae3e7ece9b9225ea
jyajoo <jyajoo1020@gmail.com>
2022-03-20 22:57:57 +0900

    add: [Implementation] 상하좌우

--- ndb796/Implementation/1_Up down left and right.py
+++ ndb796/Implementation/1_Up down left and right.py
@@ -1,10 +1,8 @@
 '''
-< 상하좌우>
+< 상하좌우 > - refine
 
 - 계획서에 따라 공간을 넘어가지 않도록 조건을 설정한 후, 이동시킨다.
 '''
-# N X N 정사각형 공간 만들기
-# 계획서 입력 받기
 
 n = int(input())
 plan = list(input().split())
@@ -23,4 +21,26 @@ for i in plan:
     elif i == "L":
         if y < 1:
             y -= 1
+print(x, y)
+
+'''
+'''
+n = int(input())
+x = y = 1
+plans = list(input().split())
+
+# L, R, U, D
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+
+type = ["L", "R", "U", "D"]
+
+for plan in plans:
+    for i in range(4):
+        if plan == type[i]:
+            nx = x + dx[i]
+            ny = y + dy[i]
+    if nx < 1 or nx > n or ny < 1 or ny > n:
+        continue
+    x, y = nx, ny
 print(x, y)
