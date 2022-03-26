69f0561f24ad43ffe3a483ad187bbaf10fdc899a
jyajoo <jyajoo1020@gmail.com>
2022-03-26 16:30:21 +0900

    fix: [Greedy] 곱하기 혹은 더하기 - 반례, 코드 수정

--- ndb796/Greedy/Q2_multiply or add.py
+++ ndb796/Greedy/Q2_multiply or add.py
@@ -2,8 +2,14 @@
 < 곱하기 혹은 더하기 >
 
 - 모든 연산은 왼쪽에서부터 이루어진다는 가정
-- 주어진 수가 0이거나 result가 0인 상태인 경우, 더하기 
+- 주어진 수가 0 또는 1이거나 result가 0 또는 1인 상태인 경우, 더하기  (EX, 1 X 2, 1 + 2)
 - 그 외에는 모두 곱하는 것이 최대의 수를 구할 수 있는 방법이 된다.
+'''
+
+'''
+!! 잘못된 풀이 !!
+> if i == 0 or result == 0:  
+1인 경우 더하기 연산을 해주어야 한다.
 '''
 
 s = input()
@@ -11,25 +17,9 @@ result = 0
 
 for i in s:
     i = int(i)
-    if i == 0 or result == 0:
+    if i <= 1 or result <= 1:
         result += i
     else:
         result *= i
-
-print(result)
-
-
-''''''
-
-s = input()
-result = 0
-
-for i in range(len(s)):
-    if i == 0 or result == 0:
-        result = int(s[i])
-    elif s[i] == 0:
-        result += int(s[i])
-    else:
-        result *= int(s[i])
 
 print(result)
