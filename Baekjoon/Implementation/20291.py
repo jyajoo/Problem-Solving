416acaeb761a245e3b95f317c97eadbda40f7bf3
jyajoo <jyajoo1020@gmail.com>
2022-03-27 22:00:59 +0900

    add: [Implementation] 파일 정리

---
+++ Baekjoon/Implementation/20291.py
@@ -0,0 +1,23 @@
+'''
+< 파일 정리 >
+
+- 확장자별로 정리하여 개수 확인
+- 사전 순 정렬
+'''
+from collections import Counter
+
+n = int(input())
+ex = []                     # 확장자 담는 곳
+for _ in range(n):
+    ex.append(input().split('.')[1])
+ex = dict(Counter(ex))      # Counter를 이용하여 개수 구하기 
+
+# a = list(ex.keys())         
+# a.sort()                  
+a = sorted(ex.keys())       # 확장자명을 사전순으로 정렬
+
+
+for i in a:
+    # print(i, end = " ")
+    # print(ex[i])
+    print(i, ex[i])
