c037e8ffaa5ddeea0e2b87f4b6b3fbbd8f403491
jyajoo <jyajoo1020@gmail.com>
2022-03-27 14:48:25 +0900

    add: [Greedy] 볼링공 고르기 - 코드 추가

--- ndb796/Greedy/Q5_Bowling.py
+++ ndb796/Greedy/Q5_Bowling.py
@@ -16,3 +16,21 @@ for i in combi:
         combi.remove(i)
 
 print(len(combi))
+
+'''
+'''
+n, m = map(int, input().split())           # n : 볼링공의 개수, m : 볼링공의 최대 무게
+balls = list(map(int, input().split()))
+
+# 1부터 10까지의 무게를 담을 수 있는 리스트
+arr = [0] * 11
+
+for i in balls:
+    arr[i] += 1
+
+result = 0
+for i in range(1, m + 1):
+    n -= arr[i]             # 무게가 i인 볼링공의 개수(A가 선택할 수 있는 개수) 제외
+    result += arr[i] * n    # A가 선택한 공의 개수 * B가 선택할 수 있는 공의 개수
+
+print(result)
