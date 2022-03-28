c0beaae7e3813e34661af6cdf609fa72c1fc22ad
jyajoo <jyajoo1020@gmail.com>
2022-03-28 18:10:01 +0900

    chore: [Implemenation] 소가 길을 건너간 이유1 - 주석 수정

--- Baekjoon/Implementation/14467.py
+++ Baekjoon/Implementation/14467.py
@@ -12,10 +12,10 @@ street = dict()
 cnt = 0
 for _ in range(n):  # (소의 번호, 소의 위치)
     c, s = map(int, input().split())
-    if c not in cow:                   # 소를 관측한 적이 있는 지 확인
+    if c not in cow:                   # 소를 관측한 적이 없다면, 리스트 추가
         cow.append(c)
     else:
-        if street[c] != s:             # 관측한 적이 있다면, 위치 비교
+        if street[c] != s:             # 기존에 이미 관측한 적이 있다면, 위치 비교
             cnt += 1
     street[c] = s
 print(cnt)
