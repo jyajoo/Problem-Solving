15cb9c118bd389588343d26c676bebf4708ed3d0
jyajoo <jyajoo1020@gmail.com>
2022-04-11 23:58:59 +0900

    chore: [Implementation] 뱀 - 풀이 추가

--- ndb796/Implementation/Q5_Snake.py
+++ ndb796/Implementation/Q5_Snake.py
@@ -67,6 +67,65 @@ while True:
                 idx = ld[idx][1]
 
 '''
-책에서의 풀이
 
 '''
+
+n = int(input())
+board = [[0] * (n + 1) for _ in range(n + 1)]
+
+k = int(input())   # 사과의 개수 (1행 1열에는 사과가 없다.)
+for _ in range(k):
+    x, y = map(int, input().split())
+    board[x][y] = 1
+
+l = int(input())   # 뱀의 방향 변환 횟수
+direct = []
+for _ in range(l):
+    x, y = input().split()
+    direct.append((int(x), y))
+
+# 처음은 오른쪽을 향하고 있다. (동, 남, 서, 북)
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+
+def turn(direction, c):
+    if c == 'L':
+        direction = (direction - 1) % 4
+    else:
+        direction = (direction + 1) % 4
+    return direction
+
+def simulate():
+    x, y = 1, 1          # 뱀의 머리 위치
+    board[x][y] = 2      # 뱀이 존재하는 위치는 2로 표시
+    direction = 0        # 처음은 동쪽을 바라보고 있음
+    time = 0
+    index = 0            # 다음에 회전할 정보
+    q = [(x, y)]         # 뱀이 차지하고 있는 위치 정보
+
+    while True:
+        nx = x + dx[direction]
+        ny = y + dy[direction]
+        if nx >= 1 and nx <= n and ny >= 1 and ny <= n and board[nx][ny] != 2:
+            if board[nx][ny] == 0:
+                board[nx][ny] = 2
+                q.append((nx, ny))
+                px, py = q.pop(0)
+                board[px][py] = 0
+                
+            elif board[nx][ny] == 1:
+                board[nx][ny] = 2
+                q.append((nx, ny))
+
+        else:
+            time += 1
+            break
+
+        x, y = nx, ny
+        time += 1
+        if index < l and time == direct[index][0]:
+            direction = turn(direction, direct[index][1])
+            index += 1
+    return time
+
+print(simulate())
