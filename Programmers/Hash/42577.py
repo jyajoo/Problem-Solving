317a611c3434e3060d4ecf2c92e2edc91c050ab0
jyajoo <jyajoo1020@gmail.com>
2023-01-02 23:25:35 +0900

    [Hash] 전화번호 목록

---
+++ Programmers/Hash/42577.py
@@ -0,0 +1,35 @@
+'''
+프로그래머스 - https://school.programmers.co.kr/learn/courses/30/lessons/42577
+
+< 전화번호 목록 >
+'''
+
+def solution(phone_book):
+    phone_book.sort()
+    for i in range(len(phone_book) - 1):
+            if phone_book[i] == phone_book[i+1][:len(phone_book[i])]:
+                return False
+    return True
+
+'''
+'''
+def solution(pb):
+    pb.sort()
+    for i, j in zip(pb, pb[1:]):
+        if j.startswith(i):
+            return False
+    return True
+
+'''
+'''
+def solution(pb):
+    hash = {}
+    for i in pb:
+        hash[i] = 1
+    for i in pb:
+        temp = ""
+        for j in i:
+            temp += j
+            if temp in hash and temp != i:
+                return False
+    return True
