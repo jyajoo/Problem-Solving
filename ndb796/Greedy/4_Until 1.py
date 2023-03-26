1b0169f30a163b7fb2dc67740b36c51b204a2339
jyajoo <jyajoo1020@gmail.com>
2023-03-26 21:26:29 +0900

    [Greedy] 1이 될 때까지

--- ndb796/Greedy/4_Until 1.py
+++ ndb796/Greedy/4_Until 1.py
@@ -42,3 +42,36 @@ while True:
 # n이 k보다 작을 경우, 1이 될 때까지 빼기 연산 횟수를 더해준다.
 result = n - 1
 print(cnt)
+
+
+'''
+2023.03.26
+'''
+n, k = map(int, input().split())
+result = 0
+while True:
+    if n == 1:
+        break
+    if n % k == 0:
+        n /= k
+    else:
+        n -= 1
+    result += 1
+print(result)
+
+'''
+'''
+n, k = map(int, input().split())
+result = 0
+
+while n >= k:
+    while n % k == 0:
+        n -= 1
+        result += 1
+    n /= k
+    result += 1
+
+while n > 1:
+    n -= 1
+    result += 1
+print(result)
