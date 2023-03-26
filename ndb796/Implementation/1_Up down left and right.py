f66e781d191a6ca0af2afbf09ee657d293538b4e
jyajoo <jyajoo1020@gmail.com>
2023-03-26 21:45:49 +0900

    [Implementation] 상하좌우

--- ndb796/Implementation/1_Up down left and right.py
+++ ndb796/Implementation/1_Up down left and right.py
@@ -44,3 +44,53 @@ for plan in plans:
         continue
     x, y = nx, ny
 print(x, y)
+
+'''
+2023.03.26
+'''
+n = int(input())
+arr = list(input().split())
+x_arr = [0, 0, -1, 1]
+y_arr = [-1, 1, 0, 0]
+X = 1
+Y = 1
+for i in arr:
+    if i == 'L':
+        x = x_arr[0]
+        y = y_arr[0]
+    elif i == 'R':
+        x = x_arr[1]
+        y = y_arr[1]
+    elif i == 'U':
+        x = x_arr[2]
+        y = y_arr[2]
+    else:
+        x = x_arr[3]
+        y = y_arr[3]
+    
+    if X + x < 1 or X + x > n or Y + y < 1 or Y + y > n:
+        continue
+    else:
+        X += x
+        Y += y
+
+print(X, Y) 
+
+'''
+'''
+n = int(input())
+arr = list(input().split())
+
+X, Y = 1, 1
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+types = ['L', 'R', 'U', 'D']
+
+for i in arr:
+    for t in range(len(types)):
+        if types[t] == i:
+            if X + dx[t] < 1 or X + dx[t] > n or Y + dy[t] < 1 or Y + dy[t] > n:
+                break
+            X += dx[t]
+            Y += dy[t]
+print(X, Y)
