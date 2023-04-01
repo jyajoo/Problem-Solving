5ae2e2be964066118df4a7ad91598c53fcaa5c0a
jyajoo <jyajoo1020@gmail.com>
2023-04-01 17:34:26 +0900

    [DFS/BFS] 음료수 얼려먹기

--- ndb796/DFS_BFS/p-149.py
+++ ndb796/DFS_BFS/p-149.py
@@ -1,3 +1,9 @@
+'''
+< 음료수 얼려먹기 >
+ 
+- BFS 풀이
+'''
+
 from collections import deque
 
 n, m = map(int, input().split())
@@ -39,10 +45,31 @@ while True:
         break
 
 print(result)           
+
 '''
-4 5
-00110
-00011
-11111
-00000
+- DFS 풀이
 '''
+n, m = map(int, input().split())
+graph = []
+for _ in range(n):
+    graph.append(list(map(int, input())))
+
+def dfs(x, y):    
+    if x < 0 or x >= n or y < 0 or y >= m:
+        return False
+    if graph[x][y] == 0:
+        graph[x][y] = 1
+        dfs(x - 1, y)
+        dfs(x + 1, y)
+        dfs(x, y - 1)
+        dfs(x, y + 1)
+        return True
+    return False
+
+result = 0
+for i in range(n):
+    for j in range(m):
+        if dfs(i, j) == True:
+            result += 1
+
+print(result)
