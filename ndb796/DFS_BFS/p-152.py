9b84ea18f7a551e00c44a369bf8a8d5018811301
jyajoo <jyajoo1020@gmail.com>
2023-04-02 13:22:51 +0900

    [DFS/BFS] 미로 탈출

--- ndb796/DFS_BFS/p-152.py
+++ ndb796/DFS_BFS/p-152.py
@@ -38,6 +38,35 @@ while queue:
 print(result)
 
 '''
+'''
+from collections import deque
+
+n, m = map(int, input().split())
+graph = []
+for _ in range(n):
+    graph.append(list(map(int, input())))
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def dfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+    return graph[n-1][m-1]
+
+print(dfs(0, 0))
+
+'''
 5 6
 101010
 111111
