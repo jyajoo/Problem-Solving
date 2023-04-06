4c9d38200578a5e6d82f213452310b83002dd82b
jyajoo <jyajoo1020@gmail.com>
2023-04-07 02:35:04 +0900

    [Sort] 퀵 정렬

--- ndb796/Sort/Quick Sort.py
+++ ndb796/Sort/Quick Sort.py
@@ -35,3 +35,31 @@ def quick(arr, start, end):
 
 quick(arr, 0, len(arr) - 1)
 print(arr)
+
+'''
+'''
+arr = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+def quick(arr, start, end):
+    if start >= end:
+        return
+    pivot = start
+    left = start + 1
+    right = end
+    while left <= right:
+        while left <= right and arr[left] <= arr[pivot]:
+            left += 1
+
+        while left <= right and arr[right] >= arr[pivot]:
+            right -= 1
+
+        if left > right:
+            arr[right], arr[pivot] = arr[pivot], arr[right]
+        else:
+            arr[left], arr[right] = arr[right], arr[left]
+
+    quick(arr, start, right - 1)
+    quick(arr, right + 1, end)
+
+quick(arr, 0, len(arr) - 1)
+print(arr)
