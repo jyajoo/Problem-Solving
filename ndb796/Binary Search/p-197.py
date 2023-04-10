630e96a126ac1fd1e9da36fe32c9a02933022d20
jyajoo <jyajoo1020@gmail.com>
2023-04-10 15:20:53 +0900

    [Binary Search] 부품 찾기

--- ndb796/Binary Search/p-197.py
+++ ndb796/Binary Search/p-197.py
@@ -7,41 +7,41 @@ product = list(map(int, input().split()))
 m = int(input())
 order = list(map(int, input().split()))
 
-# def binary_search(target, start, end):
-#     if start > end:
-#         return 'no'
-#     middle = (start + end) // 2
-#     if target == product[middle]:
-#         return 'yes'
-#     elif target > product[middle]:
-#         return binary_search(target, middle + 1, end)
-#     else:
-#         return binary_search(target, start, middle - 1)
+def binary_search(target, start, end):
+    if start > end:
+        return 'no'
+    middle = (start + end) // 2
+    if target == product[middle]:
+        return 'yes'
+    elif target > product[middle]:
+        return binary_search(target, middle + 1, end)
+    else:
+        return binary_search(target, start, middle - 1)
     
 
-# for i in range(m):
-#     print(binary_search(order[i], 0, n - 1), end = " ")
+for i in range(m):
+    print(binary_search(order[i], 0, n - 1), end = " ")
 
-# '''
-# - 이진탐색(반복문)
-# '''
-# def binary_search(arr, target, start, end):
-#     while start <= end:
-#         middle = (start + end) // 2
-#         if target == arr[middle]:
-#             return 'yes'
-#         elif target > arr[middle]:
-#             start = middle + 1
-#         else:
-#             end = middle - 1
+'''
+- 이진탐색(반복문)
+'''
+def binary_search(arr, target, start, end):
+    while start <= end:
+        middle = (start + end) // 2
+        if target == arr[middle]:
+            return 'yes'
+        elif target > arr[middle]:
+            start = middle + 1
+        else:
+            end = middle - 1
         
-#     return None
-# for i in range(m):
-#     result = binary_search(product, order[i], 0, n - 1)
-#     if result == None:
-#         print("no", end = " ")
-#     else:
-#         print(result, end = " ")
+    return None
+for i in range(m):
+    result = binary_search(product, order[i], 0, n - 1)
+    if result == None:
+        print("no", end = " ")
+    else:
+        print(result, end = " ")
 
 '''
 - 계수 정렬
