cdea81c3321f6bbafb6017d91bee0c9f2ea1be3a
jyajoo <jyajoo1020@gmail.com>
2023-04-10 15:05:54 +0900

    [Binary Search] 부품 찾기

--- ndb796/Binary Search/p-197.py
+++ ndb796/Binary Search/p-197.py
@@ -1,5 +1,6 @@
 '''
 < 부품 찾기 >
+- 이진 탐색(재귀함수)
 '''
 n = int(input())
 product = list(map(int, input().split()))
@@ -12,7 +13,7 @@ def binary_search(target, start, end):
     middle = (start + end) // 2
     if target == product[middle]:
         return 'yes'
-    if target > product[middle]:
+    elif target > product[middle]:
         return binary_search(target, middle + 1, end)
     else:
         return binary_search(target, start, middle - 1)
@@ -20,3 +21,24 @@ def binary_search(target, start, end):
 
 for i in range(m):
     print(binary_search(order[i], 0, n - 1), end = " ")
+
+'''
+- 이진탐색(반복문)
+'''
+def binary_search(arr, target, start, end):
+    while start <= end:
+        middle = (start + end) // 2
+        if target == arr[middle]:
+            return 'yes'
+        elif target > arr[middle]:
+            start = middle + 1
+        else:
+            end = middle - 1
+        
+    return None
+for i in range(m):
+    result = binary_search(product, order[i], 0, n - 1)
+    if result == None:
+        print("no", end = " ")
+    else:
+        print(result, end = " ")
