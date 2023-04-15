60025e220476f5dc8cf9336d3eb292d5052f01ae
jyajoo <jyajoo1020@gmail.com>
2023-04-15 13:54:16 +0900

    [DP] 1로 만들기

--- ndb796/DP/p-217.py
+++ ndb796/DP/p-217.py
@@ -20,3 +20,21 @@ def one(n):
         return one(n - 1)
 
 print(one(n))
+
+'''
+- 메모이제이션 적용
+- 바텀업 방식 사용
+'''
+n = int(input())
+d = [0] * 30001
+
+for i in range(2, n + 1):
+    d[i] = d[i - 1] + 1
+    if i % 2 == 0:
+        d[i] = min(d[i], d[i // 2] + 1)
+    elif i % 3 == 0:
+        d[i] = min(d[i], d[i // 3] + 1)
+    elif i % 5 == 0:
+        d[i] = min(d[i], d[i // 5] + 1)
+    
+print(d[n])
