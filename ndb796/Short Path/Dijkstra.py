77c91872b451955ef7ac673ad23ba84a7bb56172
jyajoo <jyajoo1020@gmail.com>
2023-04-17 12:15:28 +0900

    [Short Path] 다익스트라 알고리즘

---
+++ ndb796/Short Path/Dijkstra.py
@@ -0,0 +1,65 @@
+"""
+< 간단한 다익스트라 알고리즘 >
+"""
+import sys
+
+input = sys.stdin.readline
+INF = int(1e9)
+
+n, m = map(int, input().split())
+start = int(input())
+graph = [[] for _ in range(n + 1)]
+visited = [False] * (n + 1)
+distance = [INF] * (n + 1)
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+
+
+def get_smallest_node():
+    min_val = INF
+    index = 0
+    for i in range(1, n + 1):
+        if distance[i] < min_val and not visited[i]:
+            min_val = distance[i]
+            index = i
+    return index
+
+
+def dijkstra(start):
+    distance[start] = 0
+    visited[start] = True
+    for b, c in graph[start]:
+        distance[b] = c
+
+    for _ in range(n - 1):
+        now = get_smallest_node()
+        visited[now] = True
+        for b, c in graph[now]:
+            distance[b] = min(distance[b], distance[now] + c)
+
+
+dijkstra(start)
+
+for i in range(1, n + 1):
+    if distance[i] == INF:
+        print("INFINITY")
+    else:
+        print(distance[i])
+
+"""
+6 11
+1 
+1 2 2
+1 3 5
+1 4 1
+2 3 3
+2 4 2
+3 2 3
+3 6 5
+4 3 3
+4 5 1
+5 3 1
+5 6 2
+"""
