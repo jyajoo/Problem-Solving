43641c5b000a9c3c129b580b6d2ecd638c10c6e4
jyajoo <jyajoo1020@gmail.com>
2023-04-26 13:09:17 +0900

    [Short Path] 개선된 다익스트라 알고리즘

---
+++ ndb796/Short Path/Dijkstra_2.py
@@ -0,0 +1,41 @@
+"""
+< 개선된 다익스트라 알고리즘 >
+- 최단 거리가 가장 짧은 노드를 선택하는 과정을 우선순위 큐로 대체
+"""
+import sys
+import heapq
+
+input = sys.stdin.readline
+INF = int(1e9)
+
+n, m = map(int, input().split())
+start = int(input())
+graph = [[] for _ in range(n + 1)]
+distance = [INF] * (n + 1)
+
+for i in range(m):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:  # 이미 처리된 적이 있다면, continue로 넘어감
+            continue
+
+        for b, c in graph[now]:
+            distance[b] = min(distance[b], distance[now] + c)
+            heapq.heappush(q, (distance[now] + c, b))
+
+
+dijkstra(start)
+
+for i in range(1, n + 1):
+    if distance[i] == INF:
+        print("INFINITY")
+    else:
+        print(distance[i])
