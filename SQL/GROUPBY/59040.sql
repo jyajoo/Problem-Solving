957074b83ed107a1623bcc4cea2f305d87b9770b
jyajoo <jyajoo1020@gmail.com>
2023-05-30 10:30:00 +0900

    [GROUPBY] 고양이와 개는 몇 마리 있을까

---
+++ SQL/GROUPBY/59040.sql
@@ -0,0 +1,9 @@
+/*
+프로그래머스 - https://school.programmers.co.kr/learn/courses/30/lessons/59040
+
+< 고양이와 개는 몇 마리 있을까 > 
+*/
+SELECT ANIMAL_TYPE, COUNT(*) AS 'count'
+FROM ANIMAL_INS
+GROUP BY ANIMAL_TYPE
+ORDER BY ANIMAL_TYPE
