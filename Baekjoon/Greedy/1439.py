999037e714dcb5187371e5549795e81d88b038bf
jyajoo <jyajoo1020@gmail.com>
2023-07-07 22:54:10 +0900

    [Greedy] 뒤집기

--- Baekjoon/Greedy/1439.py
+++ Baekjoon/Greedy/1439.py
@@ -1,7 +1,7 @@
-'''
+"""
 백준 - https://www.acmicpc.net/problem/1439
 < 뒤집기 >
-'''
+"""
 import sys
 
 input = sys.stdin.readline
@@ -14,9 +14,30 @@ one = 0
 
 for i in range(1, len(s)):
     if pre_num != s[i]:
-        if pre_num == '0':
+        if pre_num == "0":
             zero += 1
         else:
             one += 1
         pre_num = s[i]
+print(min(zero, one))
+
+"""
+"""
+s = input()
+
+zero = 0
+one = 0
+
+if s[0] == "0":
+    zero += 1
+else:
+    one += 1
+
+for i in range(len(s) - 1):
+    if s[i] != s[i + 1]:
+        if s[i + 1] == "0":
+            zero += 1
+        else:
+            one += 1
+
 print(min(zero, one))
