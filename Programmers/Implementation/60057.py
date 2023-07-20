1aba2c654ff65bccb436bf763158bb2c5dedc270
jyajoo <jyajoo1020@gmail.com>
2023-07-20 16:26:03 +0900

    [Implementation] 문자열 압축

---
+++ Programmers/Implementation/60057.py
@@ -0,0 +1,32 @@
+"""
+프로그래머스 - https://school.programmers.co.kr/learn/courses/30/lessons/60057
+
+< 문자열 압축 >
+"""
+
+
+def solution(s):
+    answer = len(s)  # 압축되지 않을 시의 문자열 길이
+
+    for step in range(1, len(s) // 2 + 1):  # 1부터 문자열 길이의 반까지 단위를 나누게 된다.
+        prev = s[0:step]  # 비교가 이루어질 이전 문자열
+        word = ""  # 최종적으로 압축된 문자열
+        count = 1
+        for i in range(step, len(s), step):  # 이전 for문에서의 단위마다 문자열을 나눌 수 있도록 한다.
+            if prev == s[i : i + step]:  # 이전 문자열인 prev와 비교
+                count += 1
+            else:
+                # count가 2 이상일 경우, count를 포함하여 압축된 문자열을 표현한다.
+                word += str(count) + prev if count >= 2 else prev
+                count = 1  # count 재설정
+                prev = s[i : i + step]  # prev 재설정
+
+        # 두번째 for문을 마치고 나서 남은 문자열을 덧붙여주어 마무리한다.
+        word += str(count) + prev if count >= 2 else prev
+        answer = min(answer, len(word))
+
+    return answer
+
+
+s = "aabbaccc"
+print(solution(s))
