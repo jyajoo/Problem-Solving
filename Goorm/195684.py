67e59949fa2c24b31c5d7d6dfd1c7f0197f9f516
jyajoo <jyajoo1020@gmail.com>
2023-08-16 15:24:26 +0900

    [Goorm] 프로젝트 매니징

---
+++ Goorm/195684.py
@@ -0,0 +1,40 @@
+'''
+구름톤 챌린지 - https://level.goorm.io/exam/195684/%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8-%EB%A7%A4%EB%8B%88%EC%A7%95/quiz/1
+
+< 프로젝트 매니징 >
+'''
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+t, m = map(int, input().split())
+for _ in range(n):
+	m += int(input())
+	t += m // 60
+	m %= 60
+	if t > 23:
+		t %= 24
+
+print(t, m)
+
+'''
+'''
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+t, m = map(int, input().split())
+time = 0
+for _ in range(n):
+	time += int(input())
+
+t = (t + time // 60) % 24
+m = m + time % 60
+if m > 59:
+	t += m // 60
+	m %= 60
+	t %= 24
+
+print(t, m)
