efa4d863c59d3cf99fbe20bf4027cd718a9f3b07
jyajoo <jyajoo1020@gmail.com>
2023-08-17 21:59:24 +0900

    [Goorm] 완벽한 햄버거 만들기

---
+++ Goorm/195686.py
@@ -0,0 +1,29 @@
+"""
+구름톤 챌린지 - https://level.goorm.io/exam/195686/%EC%99%84%EB%B2%BD%ED%95%9C-%ED%96%84%EB%B2%84%EA%B1%B0-%EB%A7%8C%EB%93%A4%EA%B8%B0/quiz/1
+
+<완벽한 햄버거 만들기>
+"""
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+K = list(map(int, input().split()))
+
+top_val = max(K)
+top = K.index(top_val)
+K1 = K[:top]
+K2 = K[top:]
+result = 0
+
+for i in K1:
+    result += i
+
+prev = K2[0]
+for i in K2:
+    result += i
+    if i > prev:
+        result = 0
+        break
+    prev = i
+print(result)
