18829feaefe0a6d45a5b231d900df2e61aef0c00
jyajoo <jyajoo1020@gmail.com>
2023-08-21 16:22:39 +0900

    [완전탐색] 문자열 나누기

---
+++ Goorm/Exhaustive search/195688.py
@@ -0,0 +1,47 @@
+"""
+구름톤 챌린지 - https://level.goorm.io/exam/195688/%EB%AC%B8%EC%9E%90%EC%97%B4-%EB%82%98%EB%88%84%EA%B8%B0/quiz/1
+
+< 문자열 나누기 >
+"""
+import sys
+from itertools import product
+
+input = sys.stdin.readline
+
+n = int(input())  # 문자열 길이
+arr = input()  # 문자열 S
+
+# (1, 1, n - 2)로 가장 큰 수는 n - 2이므로 1부터 n - 1까지로 설정
+num = [i for i in range(1, n - 1)]
+
+# 중복 순열
+prod = list(product(num, repeat=3))
+
+# 중복 순열 중 합이 n인 것만 추출
+index_list = []
+for p in prod:
+    if sum(p) == n:
+        i, j, k = p[0], p[1], p[2]
+        a = arr[:i]
+        b = arr[i : i + j]
+        c = arr[i + j :].strip()
+        index_list.append((a, b, c))
+
+# p에 중복 제거하고 담기
+p = set()
+for a, b, c in index_list:
+    p.add(a)
+    p.add(b)
+    p.add(c)
+p = list(p)
+p.sort()
+
+# p에서 인덱스 + 1 값으로 result 구하기
+result = 0
+for a, b, c in index_list:
+    a = p.index(a)
+    b = p.index(b)
+    c = p.index(c)
+    result = max(result, a + b + c + 3)
+
+print(result)
