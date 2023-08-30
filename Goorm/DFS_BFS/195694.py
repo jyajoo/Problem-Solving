ac8c78618584ef65781ec81d1701cbe75934d5b1
jyajoo <jyajoo1020@gmail.com>
2023-08-30 13:16:46 +0900

    [Goorm] 발전기

--- Goorm/DFS_BFS/195694.py
+++ Goorm/DFS_BFS/195694.py
@@ -48,3 +48,50 @@ for a in range(n):
                     #     queue.append((nx, ny))
 
 print(result)
+
+"""
+"""
+import sys
+
+input = sys.stdin.readline
+
+
+def dfs(x, y):
+    stack = [(x, y)]
+    visited[x][y] = True
+    while stack:
+        x, y = stack.pop()
+
+        for dx, dy in direction:
+            nx = x + dx
+            ny = y + dy
+
+            # 마을을 벗어난 경우 + 집이 아닌 경우 + 이미 방문한 경우
+            if (
+                nx in (-1, n)
+                or ny in (-1, n)
+                or village[nx][ny] == 0
+                or visited[nx][ny]
+            ):
+                continue
+
+            visited[nx][ny] = True
+            stack.append((nx, ny))
+
+
+n = int(input())
+village = [list(map(int, input().split())) for _ in range(n)]
+
+# 상하좌우
+direction = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+
+visited = [[False] * (n) for _ in range(n)]
+
+result = 0
+for i in range(n):
+    for j in range(n):
+        # 방문하지 않은 집이 있다면 탐색 시작
+        if village[i][j] and not visited[i][j]:
+            result += 1
+            dfs(i, j)
+print(result)
