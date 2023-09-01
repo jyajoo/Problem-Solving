96079846d0bddde120511dc375246f35c53718aa
jyajoo <jyajoo1020@gmail.com>
2023-09-01 13:47:42 +0900

    [Goorm] 작은 노드

--- Goorm/DFS_BFS/195696.py
+++ Goorm/DFS_BFS/195696.py
@@ -2,6 +2,7 @@
 구름톤 챌린지 - https://level.goorm.io/exam/195696/%EC%9E%91%EC%9D%80-%EB%85%B8%EB%93%9C/quiz/1
 
 < 작은 노드 >
+- 비재귀 dfs
 """
 import sys
 
@@ -40,3 +41,33 @@ def dfs(k):
 dfs(k)
 # print(result)
 print(visited.count(True), result[-1])
+"""
+- 재귀 dfs
+"""
+import sys
+
+sys.setrecursionlimit(10**4)
+input = sys.stdin.readline
+
+
+def dfs(k):
+    for i in sorted(graph[k]):
+        if not visited[i]:
+            visited[i] = True
+            return dfs(i)
+    else:
+        return k
+
+
+n, m, k = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+visited = [False] * (n + 1)
+visited[k] = True
+for _ in range(m):
+    s, e = map(int, input().split())
+    graph[s].append(e)
+    graph[e].append(s)
+
+visited[k] = True
+result = dfs(k)
+print(visited.count(True), result)
