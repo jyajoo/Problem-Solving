63d241fac75ebfa55e6452bf9de09a9cff0c5330
jyajoo <jyajoo1020@gmail.com>
2023-09-05 11:33:48 +0900

    [Goorm] 연합

--- Goorm/Graph/195698.py
+++ Goorm/Graph/195698.py
@@ -3,44 +3,44 @@
 
 < 연합 > 
 """
-# import sys
-# from collections import deque
+import sys
+from collections import deque
 
-# input = sys.stdin.readline
-# sys.setrecursionlimit(1000)
-
-
-# def find_parent(x):
-#     if parent[x] != x:
-#         return find_parent(parent[x])
-#     else:
-#         return x
+input = sys.stdin.readline
+sys.setrecursionlimit(1000)
 
 
-# def union_parent(s, e):
-#     s = find_parent(s)
-#     e = find_parent(e)
-#     if s < e:
-#         parent[e] = s
-#     else:
-#         parent[s] = e
+def find_parent(x):
+    if parent[x] != x:
+        return find_parent(parent[x])
+    else:
+        return x
 
 
-# n, m = map(int, input().split())
-# graph = [[] for _ in range(n + 1)]
-# parent = [i for i in range(n + 1)]
-# for _ in range(m):
-#     s, e = map(int, input().split())
-#     graph[s].append(e)
-#     if s in graph[e]:
-#         union_parent(s, e)
+def union_parent(s, e):
+    s = find_parent(s)
+    e = find_parent(e)
+    if s < e:
+        parent[e] = s
+    else:
+        parent[s] = e
 
-# result = []
-# for i in range(1, n + 1):
-#     if find_parent(i) not in result:
-#         result.append(find_parent(i))
 
-# print(len(result))
+n, m = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+parent = [i for i in range(n + 1)]
+for _ in range(m):
+    s, e = map(int, input().split())
+    graph[s].append(e)
+    if s in graph[e]:
+        union_parent(s, e)
+
+result = []
+for i in range(1, n + 1):
+    if find_parent(i) not in result:
+        result.append(find_parent(i))
+
+print(len(result))
 
 """
 """
