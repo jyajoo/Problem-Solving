2b41262f5c1790a99a11a0793d15f882eea2e894
jyajoo <jyajoo1020@gmail.com>
2023-12-17 15:35:05 +0900

    [BOJ] N과 M (4)

--- Baekjoon/Backtracking/15652.py
+++ Baekjoon/Backtracking/15652.py
@@ -14,3 +14,21 @@ lst = list(combinations_with_replacement(numbers, m))
 
 for i in lst:
     print(*i)
+
+"""
+"""
+import sys
+
+def dfs(arr, x):
+    if len(arr) == m:
+        print(*arr)
+        return
+    
+    for i in range(x, n + 1):
+        dfs(arr + [i], i)
+
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+dfs([], 1)
