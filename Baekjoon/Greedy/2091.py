e7928e808383e28388faf0d4a37dc19992434c2b
jyajoo <jyajoo1020@gmail.com>
2024-01-21 00:48:52 +0900

    [BOJ] 동전

--- Baekjoon/Greedy/2091.py
+++ Baekjoon/Greedy/2091.py
@@ -8,8 +8,8 @@ import sys
 input = sys.stdin.readline
 
 x, a, b, c, d = map(int, input().split())
-unit_index = {1 : 0, 5 : 1, 10 : 2, 25 : 3}
-unit_coins = {1 : a, 5 : b, 10 : c, 25 : d}
+unit_index = {1: 0, 5: 1, 10: 2, 25: 3}
+unit_coins = {1: a, 5: b, 10: c, 25: d}
 # dp[i][j] = i원일 때, j는 동전 개수(각 단위의 동전 개수 + 총 동전 개수)
 dp = [[-1] * 5 for _ in range(x + 1)]
 for i in range(5):
@@ -73,6 +73,48 @@ for i in range(d + 1):
     if money == x and sum(coins) < min_count:
         min_count = sum(coins)
         min_coins = copy.deepcopy(coins)
+
+answer = [0] * 4
+
+if min_count != int(1e9):
+    for i in range(4):
+        _, c = coins_count[i]
+        answer[i] = c - min_coins[i]
+
+print(*answer)
+"""
+"""
+import sys
+
+input = sys.stdin.readline
+import copy
+
+x, a, b, c, d = map(int, input().split())
+total_money = a + 5 * b + 10 * c + 25 * d
+coins_count = [(1, a), (5, b), (10, c), (25, d)]
+
+min_count = int(1e9)
+min_coins = [0] * 4
+for i in range(d + 1):
+    money = total_money - 25 * i
+    coins = [0] * 4
+    coins[3] = i
+
+    k = money - x # 최소의 동전으로 k원 만들기
+    if k < 0:
+        continue
+    # 10, 5, 1의 순서로 동전이 최소가 되도록 빼준다
+    for j in [2, 1, 0]:
+        unit, coin_count = coins_count[j]
+        count = k // unit
+        if count > coin_count:
+            count = coin_count
+        k = k - count * unit
+        coins[j] = count
+        if k == 0 and sum(coins) < min_count:
+            min_count = sum(coins)
+            min_coins = copy.deepcopy(coins)
+            break
 
 answer = [0] * 4
 
