f5722034685cb4d016eb1349b5c7664f130ebc50
jyajoo <jyajoo1020@gmail.com>
2024-02-21 16:53:40 +0900

    [SQL] String, Date

---
+++ SQL/STRING_DATE/59047.sql
@@ -0,0 +1,10 @@
+/*
+프로그래머스 - https://school.programmers.co.kr/learn/courses/30/lessons/59047
+
+< 이름에 el이 들어가는 동물 찾기 >
+*/
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS 
+WHERE NAME LIKE '%EL%'
+AND ANIMAL_TYPE = 'Dog'
+ORDER BY NAME
