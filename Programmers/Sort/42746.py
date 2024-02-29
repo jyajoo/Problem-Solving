834fb6b2c9f31711805a34bb75e291977fd7e81e
jyajoo <jyajoo1020@gmail.com>
2024-02-29 17:32:54 +0900

    [PRO] 가장 큰 수

--- Programmers/Sort/42746.py
+++ Programmers/Sort/42746.py
@@ -1,9 +1,11 @@
-'''
+"""
 프로그래머스 - https://school.programmers.co.kr/learn/courses/30/lessons/42746
 
 < 가장 큰 수 >
-'''
+"""
+
 import functools
+
 
 def comparator(a, b):
     if int(a + b) > int(b + a):
@@ -11,7 +13,21 @@ def comparator(a, b):
     else:
         return 1
 
+
 def solution(numbers):
     numbers = list(map(str, numbers))
-    numbers = sorted(numbers, key = functools.cmp_to_key(comparator))
-    return str(int(''.join(numbers)))
+    numbers = sorted(numbers, key=functools.cmp_to_key(comparator))
+    return str(int("".join(numbers)))
+
+
+"""
+"""
+
+
+def solution(numbers):
+    answer = ""
+    numbers = list(map(str, numbers))
+    numbers.sort(key=lambda x: x * 3, reverse=True)
+    answer = str(int("".join(numbers)))
+
+    return answer
