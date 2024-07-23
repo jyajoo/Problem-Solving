5a95c345df366d232cf1e9d2fad6ec26d40007af
jyajoo <jyajoo1020@gmail.com>
2024-07-23 09:34:11 +0900

    [BOJ] 게임을 만든 동준이

---
+++ Baekjoon/Greedy/2847.py
@@ -0,0 +1,27 @@
+"""
+백준 - https://www.acmicpc.net/problem/2847
+
+< 게임을 만든 동준이 >
+"""
+
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+num = [int(input()) for _ in range(n)]
+
+result = 0
+score = num[-1] - 1
+for i in range(n - 2, -1, -1):
+    if num[i] < score:
+        score = num[i]
+
+    if num[i] >= score:
+        result += num[i] - score
+        num[i] = score
+        score -= 1
+
+    if score < 1:
+        score = 1
+print(result)
