a16a08cdd2f0bd20cb16f377681d68212e74ce68
jyajoo <jyajoo1020@gmail.com>
2024-07-24 22:33:31 +0900

    [BOJ] 볼 모으기

--- Baekjoon/Greedy/17615.py
+++ Baekjoon/Greedy/17615.py
@@ -3,6 +3,7 @@
 
 < 볼 모으기 >
 """
+
 import sys
 
 input = sys.stdin.readline
@@ -55,3 +56,60 @@ if r_count == 0 or b_count == 0:
 else:
     result = min(left_red, left_blue, right_red, right_blue)
     print(result)
+
+"""
+"""
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+arr = list(input().strip())
+r_balls = []
+b_balls = []
+
+ex_ball = arr[0]
+cnt = 1
+for i in range(1, n):
+    if ex_ball == arr[i]:
+        cnt += 1
+    else:
+        if arr[i] == "B":
+            r_balls.append(cnt)
+        else:
+            b_balls.append(cnt)
+        ex_ball = arr[i]
+        cnt = 1
+
+if arr[-1] == "B":
+    b_balls.append(cnt)
+else:
+    r_balls.append(cnt)
+
+result = int(1e9)
+if len(r_balls) == 0 or len(b_balls) == 0:
+    result = 0
+else:
+    # 오른쪽 끝 R
+    if arr[-1] == "R":
+        cnt_r = sum(r_balls[: len(r_balls) - 1])
+        cnt_b = sum(b_balls)
+        result = min(result, cnt_r, cnt_b)
+
+    # 오른쪽 끝 B
+    else:
+        cnt_r = sum(r_balls)
+        cnt_b = sum(b_balls[: len(b_balls) - 1])
+        result = min(result, cnt_r, cnt_b)
+
+    # 왼쪽 끝 R
+    if arr[0] == "R":
+        cnt_r = sum(r_balls[1:])
+        cnt_b = sum(b_balls)
+        result = min(result, cnt_r, cnt_b)
+    else:
+        cnt_r = sum(r_balls)
+        cnt_b = sum(b_balls[1:])
+        result = min(result, cnt_r, cnt_b)
+
+print(result)
