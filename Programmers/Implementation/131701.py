cdfab7c1c8a68611501c218751b6222c05b130d0
jyajoo <jyajoo1020@gmail.com>
2024-09-25 09:33:50 +0900

    [PRO] 연속 부분 수열 합의 개수

---
+++ Programmers/Implementation/131701.py
@@ -0,0 +1,19 @@
+"""
+프로그래머스 - https://school.programmers.co.kr/learn/courses/30/lessons/131701
+
+< 연속 부분 수열 합의 개수 >
+"""
+
+
+def solution(elements):
+    length = len(elements)
+    result = set()
+    for i in range(1, length + 1):
+        for start in range(length):
+            end = start + i
+            if end < length:
+                n = sum(elements[start:end])
+            else:
+                n = sum(elements[start:] + elements[: end % length])
+            result.add(n)
+    return len(result)
