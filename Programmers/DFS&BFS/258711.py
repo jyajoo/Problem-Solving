7b22991dd4ac21706db1304702e172f016dc069d
jyajoo <jyajoo1020@gmail.com>
2025-01-22 20:28:20 +0900

    [PRO] 도넛과 막대 그래프

--- Programmers/DFS&BFS/258711.py
+++ Programmers/DFS&BFS/258711.py
@@ -49,3 +49,56 @@ def solution(edges):
         else:
             c += 1
     return [created_node, a, b, c]
+
+'''
+'''
+from collections import defaultdict
+import sys
+
+sys.setrecursionlimit(10**6)
+
+def solution(edges):
+    def dfs(now):
+        nonlocal nc, ec
+        visited.add(now) 
+        nc += 1 
+        ec += len(graph[now]) 
+        
+        for i in graph[now]:
+            if i not in visited: 
+                dfs(i)
+    
+    answer = [0, 0, 0, 0]
+    
+    graph = defaultdict(list)  
+    in_degree = defaultdict(int)  # 해당 노드로 들어오는 간선 개수
+    out_degree = defaultdict(int)  # 해당 노드에서 나가는 간선 개수
+    
+    for s, e in edges:
+        in_degree[e] += 1
+        out_degree[s] += 1
+        graph[s].append(e)
+        
+    create_node = 0
+    count = 0
+    for v, e in out_degree.items():
+        if in_degree[v] == 0 and count < e:
+            count = e
+            create_node = v
+    
+    answer[0] = create_node
+    
+    visited = set()
+    for now in graph[create_node]:
+        if now not in visited:  # 방문되지 않은 노드만 탐색
+            nc, ec = 0, 0
+            dfs(now)
+            
+            if nc == ec:  # 도넛 모양 그래프
+                answer[1] += 1
+            elif nc == ec + 1:  # 막대 모양 그래프
+                answer[2] += 1
+            else:  # 8자 모양 그래프
+                answer[3] += 1
+                
+    return answer
