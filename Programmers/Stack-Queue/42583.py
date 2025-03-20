cb74233c62c4d72a9a396ea5935affc993a56695
jyajoo <jyajoo1020@gmail.com>
2025-03-20 12:28:02 +0900

    [PRO] 다리를 지나는 트럭

--- Programmers/Stack-Queue/42583.py
+++ Programmers/Stack-Queue/42583.py
@@ -24,3 +24,31 @@ def solution(bridge_length, weight, truck_weights):
         time += 1
     time += bridge_length    
     return time
+
+'''
+'''
+from collections import deque
+def solution(bridge_length, weight, truck_weights):
+    answer = 0
+    bridge = deque([0] * bridge_length)
+    sum_weight = 0
+    truck_weights = deque(truck_weights)
+    
+    while truck_weights:
+        answer += 1
+        w = bridge.popleft()
+        sum_weight -= w
+        tw = truck_weights[0]
+        if (tw + sum_weight) <= weight:
+            tw = truck_weights.popleft()
+            bridge.append(tw)
+            sum_weight += tw
+        else:
+            bridge.append(0)
+    
+    for idx, x in enumerate(list(bridge)[::-1]):
+        if x != 0:
+            answer += bridge_length - idx
+            break
+    
+    return answer
