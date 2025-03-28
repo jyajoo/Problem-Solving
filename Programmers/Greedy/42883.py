d4a341433113679bc9c484f549f88b14d5cbae16
jyajoo <jyajoo1020@gmail.com>
2025-03-28 18:08:20 +0900

    [PRO] 큰 수 만들기

--- Programmers/Greedy/42883.py
+++ Programmers/Greedy/42883.py
@@ -35,3 +35,39 @@ def solution(number, k):
         stack = stack[:-k]
 
     return "".join(stack)
+'''
+25.03.28
+'''
+def solution(number, k):
+    number = list(map(int, number))
+    num_list = []
+    cnt = 0
+    for n in number:
+        if num_list:
+            for _ in range(len(num_list)):
+                x = num_list[-1]
+                if x < n and cnt < k: # 새로운 수가 더 클 때, 기존에 더 작았던 수를 제외시킨다.
+                    num_list.pop()
+                    cnt += 1
+                else:
+                    break
+            num_list.append(n)
+        else:
+            num_list.append(n)
+    
+    if cnt != k:
+        num_list = num_list[:-(k - cnt)]
+    return ''.join(map(str, num_list))
+'''
+'''
+def solution(number, k):
+    num_list = [number[0]]
+    for n in number[1:]:
+        while num_list and num_list[-1] < n and k > 0:
+            num_list.pop()
+            k -= 1
+        num_list.append(n)
+    
+    if k > 0:
+        num_list = num_list[:-k]
+    return ''.join(num_list)
