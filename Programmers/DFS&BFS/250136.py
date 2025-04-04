597aaa880e70bbf9eb882b00ff211bbcd910d591
jyajoo <jyajoo1020@gmail.com>
2025-04-04 17:43:42 +0900

    [PRO] 석유 시추

--- Programmers/DFS&BFS/250136.py
+++ Programmers/DFS&BFS/250136.py
@@ -58,3 +58,54 @@ def solution(l):
                     count += size[num]
         result = max(result, count)
     return result
+'''
+'''
+from collections import deque
+def solution(land):
+    def bfs(i, j, room_number):
+        nonlocal visited
+        q = deque()
+        q.append((i, j))
+        visited[i][j] = True
+        land[i][j] = room_number
+        direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        count = 1
+        while q:
+            x, y = q.popleft()
+            
+            for dx, dy in direction:
+                nx = x + dx
+                ny = y + dy
+                if nx < 0 or nx >= len(land) or ny < 0 or ny >= len(land[0]):
+                    continue
+                if not visited[nx][ny] and land[nx][ny]:
+                    q.append((nx, ny))
+                    visited[nx][ny] = True
+                    count += 1
+                    land[nx][ny] = room_number
+        return count
+                    
+            
+    answer = 0
+    room_number = 1
+    rooms = [0]
+    visited = [[False] * len(land[0]) for _ in range(len(land))]
+    for i in range(len(land)):
+        for j in range(len(land[0])):
+            if not visited[i][j] and land[i][j]:
+                count = bfs(i, j, room_number)
+                rooms.append(count)
+                room_number += 1
+    for j in range(len(land[0])):
+        room_numbers = set()
+        for i in range(len(land)):
+            if land[i][j] != 0:
+                room_numbers.add(land[i][j])
+        
+        result = 0
+        for i in room_numbers:
+            result += rooms[i]
+        answer = max(answer, result)
+    
+    
+    return answer
