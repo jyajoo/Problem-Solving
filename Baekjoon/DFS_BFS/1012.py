23157e0cf5d446961cc458d2ccbe4a9ef5afaefb
jyajoo <jyajoo1020@gmail.com>
2025-10-07 13:30:47 +0900

    [BOJ] 유기농 배추

--- Baekjoon/DFS_BFS/1012.py
+++ Baekjoon/DFS_BFS/1012.py
@@ -8,11 +8,11 @@
 
 1 <= m, n <= 50 가로세로
 1 <= k <= 2500 배추의 개수
-
+시간 복잡도 <= O(N^2)
 '''
 '''
 DFS로 인접한 배추 탐색
-시간복잡도는 O(N^2)
+시간 복잡도는 O(MN)
 '''
 import sys
 
@@ -48,4 +48,38 @@ for _ in range(t):
             if board[a][b] == 1 and not visited[a][b]:
                 dfs(a, b)
                 result += 1
+    print(result)
+'''
+DFS로 인접한 배추 탐색(재귀)
+'''
+import sys
+sys.setrecursionlimit(10**5)
+input = sys.stdin.readline
+
+def dfs(x, y):
+    global visited
+    for dx, dy in delta:
+        nx = x + dx
+        ny = y + dy
+        if 0 <= nx < n and 0 <= ny < m and board[nx][ny] and not visited[nx][ny]:
+            visited[nx][ny] = True
+            dfs(nx, ny)
+
+t = int(input())
+delta = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+for _ in range(t):
+    m, n, k = map(int, input().split())
+    board = [[0] * m for _ in range(n)]
+    visited = [[False] * m for _ in range(n)]
+    result = 0
+    for _ in range(k):
+        x, y = map(int, input().split())
+        board[y][x] = 1
+
+    for x in range(n):
+        for y in range(m):
+            if board[x][y] and not visited[x][y]:
+                visited[x][y] = True
+                result += 1
+                dfs(x, y)
     print(result)
